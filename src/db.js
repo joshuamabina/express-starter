d460ceccb11d3e071fc82d4a13a160300b91844a
Joshua Mabina <mabinajoshua@gmail.com>
2019-07-04 10:49:12 +0300

    style: add space after comment token

--- src/db.js
+++ src/db.js
@@ -10,6 +10,6 @@ mongoose.connect(uri, options);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Database connection error:')); // eslint-disable-line no-console
-db.on('disconnected', console.error.bind(console, 'Database disconnected:')); //eslint-disable-line no-console
+db.on('disconnected', console.error.bind(console, 'Database disconnected:')); // eslint-disable-line no-console
 
 export default db;
