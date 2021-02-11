bd585f1a3fdde1044a814522607db86450f4c748
Joshua Mabina <mabinajoshua@gmail.com>
2021-02-11 20:42:26 +0300

    clean up

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,36 +1,34 @@
-import { Router } from 'express';
-import Africastalking from 'africastalking';
-import customId from 'custom-id'
+import { Router } from "express";
+import Africastalking from "africastalking";
+import customId from "custom-id";
 
 const router = Router();
 const AT = Africastalking({
-  apiKey: 'cbe49b692bacebcd1e96085eac4098f9e8fea7e0438b5d650de88fd8b1b32b6b',
-  username: 'sandbox',
+  apiKey: process.env.AT_API_KEY,
+  username: process.env.AT_USERNAME,
 });
 
 router.post("/register", (req, res) => {
   // TODO extract name and age
   // from req.body.text line by line
-
-  const lines = req.body.text.trim().split('\n');
+  const lines = req.body.text.trim().split("\n");
   const responses = lines.map(line => customId({ name: line }));
 
   const sms = AT.SMS;
 
   const opts = {
-    to: '+255783787166',
-    from: '18369',
-    message: responses.toString(),
+    to: process.env.AT_PHONE_TO,
+    from: process.env.AT_PHONE_FROM,
+    message: responses.toString()
   };
 
   //Configure options for message sending
-  sms.send(opts)
-    .then(function(success) {
-      console.log(success);
+  sms
+    .send(opts)
+    .then(function (success) {
       res.send({ opts, sms, success });
     })
-    .catch(function(error) {
-      console.log(error);
+    .catch(function (error) {
       res.send({ opts, sms, error });
     });
 });
